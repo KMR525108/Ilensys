16ed364d94366adb801c2ee6f054b66921d0543d
Your Name <you@example.com>
2021-02-26 15:11:17 +0530

    First Commit

---
+++ Ilensysproject/src/test/java/com/Ilensys/testscripts/TC_101.java
@@ -0,0 +1,26 @@
+package com.Ilensys.testscripts;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class TC_101 {
+
+	public static void main(String[] args) throws InterruptedException {
+		WebDriverManager.chromedriver().setup();
+        ChromeDriver driver = new ChromeDriver();
+		//WebDriverManager.firefoxdriver().setup();
+		//FirefoxDriver driver = new FirefoxDriver();
+        //WebDriverManager.edgedriver().setup();
+        //EdgeDriver driver = new EdgeDriver();
+        driver.get("https://test.salesforce.com/");
+	
+        driver.manage().window().maximize();
+        driver.findElement(By.id("username")).sendKeys("uttam@trescent-uat.com");;
+		driver.findElement(By.id("password")).sendKeys("Trescent@123");
+		driver.findElement(By.id("Login")).click();
+		Thread.sleep(100000);
+		System.out.println("test");
+		
+   }
+}
